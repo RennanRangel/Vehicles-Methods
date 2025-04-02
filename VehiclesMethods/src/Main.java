e92572f276c96cf436f92b22fc0cf0ef0cc25f25
RennanRangel <rennano965@gmail.com>
2025-04-02 14:12:49 -0300

    Add files via upload

---
+++ VehiclesMethods/src/Main.java
@@ -0,0 +1,12 @@
+public class ATVIZA {
+
+
+    public static void main(String[] args) {
+
+        Veiculo veiculo = new Veiculo("Lamborghini Aventador", 2023);
+        veiculo.exibirinformacoes();
+        Carro carro = new Carro ("Ferrari", 2025, 2);
+        carro.exibirinformacoes();
+
+    }
+}
